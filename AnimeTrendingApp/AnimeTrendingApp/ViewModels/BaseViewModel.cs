a1aabf4b2e46070122958e58d0dda3915bf34d38
Fernando Mariano Pereira <fernandovmp@outlook.com>
2019-10-30 19:45:38 -0300

    Added base view model class

---
+++ AnimeTrendingApp/AnimeTrendingApp/ViewModels/BaseViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace AnimeTrendingApp.ViewModels
+{
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string property = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+
+        protected bool SetProperty<T>(ref T attribute, T value, [CallerMemberName] string property = "")
+        {
+            if(EqualityComparer<T>.Default.Equals(attribute, value))
+            {
+                return false;
+            }
+            attribute = value;
+            OnPropertyChanged(property);
+            return true;
+        }
+    }
+}
