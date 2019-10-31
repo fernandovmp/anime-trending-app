f3acf96e49cac01478de2a4ee7fbc0b4d3bca26f
Fernando Mariano Pereira <fernandovmp@outlook.com>
2019-10-31 14:53:29 -0300

    Remove medium  property and add original property

--- AnimeTrendingApp/AnimeTrendingApp/Models/PosterImage.cs
+++ AnimeTrendingApp/AnimeTrendingApp/Models/PosterImage.cs
@@ -8,7 +8,7 @@ namespace AnimeTrendingApp.Models
     {
         public string Tiny { get; set; }
         public string Small { get; set; }
-        public string Medium { get; set; }
+        public string Original { get; set; }
         public string Large { get; set; }
     }
 }
