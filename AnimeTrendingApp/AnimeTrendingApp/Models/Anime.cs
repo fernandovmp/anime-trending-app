382600cf3ca2e058a1e44c6c36bb34a37d68ab2b
Fernando Mariano Pereira <fernandovmp@outlook.com>
2019-10-31 15:14:09 -0300

    Define int properties as nullables and add average rating property

--- AnimeTrendingApp/AnimeTrendingApp/Models/Anime.cs
+++ AnimeTrendingApp/AnimeTrendingApp/Models/Anime.cs
@@ -8,10 +8,11 @@ namespace AnimeTrendingApp.Models
     {
         public string CanonicalTitle { get; set; }
         public string Synopsis { get; set; }
-        public int PopularityRank { get; set; }
-        public int RatingRank { get; set; }
+        public int? PopularityRank { get; set; }
+        public int? RatingRank { get; set; }
+        public float? AverageRating { get; set; }
         public string Status { get; set; }
-        public int EpisodeCount { get; set; }
+        public int? EpisodeCount { get; set; }
         public PosterImage PosterImage { get; set; }
         public PosterImage CoverImage { get; set; }
     }
